4a16694664039500d1664ac4896d9f33ab2e7e89
PradnyaKulkarni2005 <kulkarnipradnya1245@gmail.com>
2025-03-27 23:46:22 +0530

    added the company ,job and job_application section

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :jobs
+  resources :job_applications
+  resources :companies
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_25_092520) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_25_110154) do
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.integer "industry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "job_applications", force: :cascade do |t|
+    t.string "status"
+    t.datetime "applied_on"
+    t.integer "job_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["job_id"], name: "index_job_applications_on_job_id"
+    t.index ["user_id"], name: "index_job_applications_on_user_id"
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "location"
+    t.integer "company_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_jobs_on_company_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "password"
@@ -20,4 +48,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_25_092520) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "job_applications", "jobs"
+  add_foreign_key "job_applications", "users"
+  add_foreign_key "jobs", "companies"
 end
